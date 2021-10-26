196f0430ade5f4198859ba168d470ad015c1a970
hermann3400 <hermann3400@hotmail.com>
2021-10-26 09:00:19 +0200

    Adding articles and updating db

--- database.sql
+++ database.sql
@@ -2365,6 +2365,7 @@ COPY public.articles (id, title, description, url, image_url, profile, scraped, 
 2319	NSW government stands up dedicated unit to fight identity theft | ZDNet	IDSupport NSW will be the single point-of-contact for citizens who have had their identity compromised.	https://www.zdnet.com/article/nsw-government-stands-up-dedicated-unit-to-fight-identity-theft/	https://www.zdnet.com/a/img/resize/640b9c994b261942902ac90672b2faffd3a78911/2021/06/25/f794d6d9-9bda-4807-b5e2-eb048305b26b/identity-access-management.jpg?width=770&height=578&fit=crop&auto=webp	zdnet	t	2021-10-26 03:00:02.248155+02	2021-10-25 23:52:47+02	Aimee Chanthadavong	zdnet/NSW-government-stands-up-dedicated-unit-to-fight-identity-theft
 2320	Home Affairs in talks to give telco more blocking powers against malicious messages | ZDNet	At Senate Estimates, Home Affairs secretary Mike Pezzullo reiterated his department would be more aggressive in addressing cyber threats.	https://www.zdnet.com/article/home-affairs-in-talks-to-give-telco-more-blocking-powers-against-malicious-messages/	https://www.zdnet.com/a/img/resize/643b2af1d04815b9e703c7534fd25133eb75af8d/2021/10/26/4e176b83-28a8-491d-a7bb-45bc341a65e3/gettyimages-1130417448.jpg?width=770&height=578&fit=crop&auto=webp	zdnet	t	2021-10-26 05:00:02.509259+02	2021-10-26 02:39:29+02	Campbell Kwan	zdnet/Home-Affairs-in-talks-to-give-telco-more-blocking-powers-against-malicious-messages
 2321	KT clarifies routing error caused outage instead of DDoS attack | ZDNet	South Korea's IT ministry is still investigating what caused KT's nationwide outage of their network.	https://www.zdnet.com/article/kt-clarifies-routing-error-caused-outage-instead-of-ddos-attack/	https://www.zdnet.com/a/img/resize/eadefe93ab6b869c9ea906c5e8d16136f75779a5/2021/10/26/a79849a2-df35-445e-9a71-7b576ea00f5b/gettyimages-149609835.jpg?width=770&height=578&fit=crop&auto=webp	zdnet	t	2021-10-26 06:00:02.655991+02	2021-10-26 03:19:49+02	Cho Mu-Hyun	zdnet/KT-clarifies-routing-error-caused-outage-instead-of-DDoS-attack
+2322	Ukrainian police detain gang who laundered funds for Russian hacking groups	Ukraine's national police detained suspects on Monday for stealing funds from cryptocurrency wallets and laundering profits for cybercrime organizations.	https://therecord.media/ukrainian-police-detain-gang-who-laundered-funds-for-russian-hacking-groups/	https://therecord.media/wp-content/uploads/2021/10/Ukraine-cyber-arrests.jpg	therecord	t	2021-10-26 09:00:02.162068+02	2021-10-26 08:21:49+02	Catalin Cimpanu	therecord/Ukrainian-police-detain-gang-who-laundered-funds-for-Russian-hacking-groups
 \.
 
 
@@ -2372,7 +2373,7 @@ COPY public.articles (id, title, description, url, image_url, profile, scraped, 
 -- Name: articles_id_seq; Type: SEQUENCE SET; Schema: public; Owner: osinter_admin
 --
 
-SELECT pg_catalog.setval('public.articles_id_seq', 2321, true);
+SELECT pg_catalog.setval('public.articles_id_seq', 2322, true);
 
 
 --
---
+++ therecord/Ukrainian-police-detain-gang-who-laundered-funds-for-Russian-hacking-groups.md
@@ -0,0 +1,39 @@
+# Ukrainian police detain gang who laundered funds for Russian hacking groups
+### 
+
+## Information:
++ Source: The Record
++ Link: [article](https://therecord.media/ukrainian-police-detain-gang-who-laundered-funds-for-russian-hacking-groups/)
++ Date: October 26, 2021
++ Author: Catalin Cimpanu
+
+
+## Article:
+![Ukrainian police detain gang who laundered funds for Russian hacking groups](https://therecord.media/wp-content/uploads/2021/10/Ukraine-cyber-arrests.jpg)
+
+Ukraine’s national police detained suspects on Monday for stealing funds from cryptocurrency wallets and laundering profits for cybercrime organizations.
+
+
+The arrests took place as part of a joint investigation with US authorities, the Ukrainian National Police (NPU) said in a [press release](https://www.npu.gov.ua/news/kiberzlochini/ukrajinski-pravooxoronczi-blokuvali-diyalnist-chleniv-mizhnarodnogo-transnaczionalnogo-xakerskogo-ugrupovannya/) on Monday.
+
+
+An undisclosed number of suspects were detained following house searchers across the country.
+
+
+Authorities said they seized both hardware resources and software programs used to infect users with malware and launder criminal proceeds.
+
+
+“During the investigation, police found that the suspects were disguised under various nicknames on the Darknet network,” the NPU said on Monday.
+
+
+“They used an extensive network of fictitious personas, as well as financial systems, and conducted complex operations to launder criminal proceeds in the interests of cybercriminals.”
+
+
+A spokesperson for the US Department of Justice did not return an email sent on Monday seeking additional information on the investigation.
+
+
+
+
+
+#### Tags:
+[[]] [[The Record]]
