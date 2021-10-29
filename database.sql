452d36afd64b11a69ee7f16bda63a36394708c70
hermann3400 <hermann3400@hotmail.com>
2021-10-29 23:00:18 +0200

    Adding articles and updating db

--- database.sql
+++ database.sql
@@ -2492,6 +2492,7 @@ COPY public.articles (id, title, description, url, image_url, profile, scraped, 
 2446	Let's Encrypt explains last month's outages caused by certificate expiration | ZDNet	Fortinet, Shopify and others reported issues last month thanks to the expiration of a root certificate provided by Let's Encrypt.	https://www.zdnet.com/article/lets-encrypt-explains-those-outages-last-month/	https://www.zdnet.com/a/img/resize/43a68c0e3d4cda383d88eae167ca61fa2f4ebab0/2021/10/29/dba79a33-2f60-44ae-80dd-968902476e38/lets-encrpyt.jpg?width=770&height=578&fit=crop&auto=webp	zdnet	t	2021-10-29 20:00:02.181907+02	2021-10-29 17:37:00+02	Jonathan Greig	zdnet/Lets-Encrypt-explains-last-months-outages-caused-by-certificate-expiration
 2447	The best free VPNs: Why they don't exist | ZDNet	If you're not paying for your VPN service, where is the provider getting the money to run it? The answer might cause you to lose some sleep.	https://www.zdnet.com/article/the-best-free-vpns-why-they-dont-exist/	https://www.zdnet.com/a/img/resize/0bee2bb0eef4957f0627e2e3cae916847f8b7332/2018/05/23/7a517a96-8b45-487d-a5a7-4d59e4c4b7c1/gettyimages-111035051.jpg?width=770&height=578&fit=crop&auto=webp	zdnet	t	2021-10-29 21:00:01.997157+02	2021-10-29 18:35:00+02	David Gewirtz	zdnet/The-best-free-VPNs-Why-they-dont-exist
 2448	Snake malware biting hard on 50 apps for only $25	Cybercriminals are flooding to use the Snake password-stealing trojan, making it one of the popular malware families used in attacks.	https://www.bleepingcomputer.com/news/security/snake-malware-biting-hard-on-50-apps-for-only-25/	https://www.bleepstatic.com/content/hl-images/2021/10/29/green_snake.jpg	bleepingcomputer	t	2021-10-29 22:00:02.078665+02	2021-10-29 18:20:10+02	Bill Toulas	bleepingcomputer/Snake-malware-biting-hard-on-50-apps-for-only-25
+2449	TA575 criminal group using 'Squid Game' lures for Dridex malware | ZDNet	The emails come with subject lines saying things like Squid Game is back, watch new season before anyone else, or pretend to offer victims a spot in the cast of the show's second season.	https://www.zdnet.com/article/ta575-criminal-group-using-squid-game-lures-for-dridex-malware/	https://www.zdnet.com/a/img/resize/faf28d337e78e40c5be723bd96e94b2f0b1fce42/2021/10/29/4965c83d-5cb3-401a-b04f-6807a6a9200a/shutterstock-2056553015.jpg?width=770&height=578&fit=crop&auto=webp	zdnet	t	2021-10-29 23:00:02.700917+02	2021-10-29 20:36:52+02	Jonathan Greig	zdnet/TA575-criminal-group-using-Squid-Game-lures-for-Dridex-malware
 \.
 
 
@@ -2499,7 +2500,7 @@ COPY public.articles (id, title, description, url, image_url, profile, scraped, 
 -- Name: articles_id_seq; Type: SEQUENCE SET; Schema: public; Owner: osinter_admin
 --
 
-SELECT pg_catalog.setval('public.articles_id_seq', 2448, true);
+SELECT pg_catalog.setval('public.articles_id_seq', 2449, true);
 
 
 --
